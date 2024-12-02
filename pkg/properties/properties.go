56524c5b8cbb8554bb121b4351ca0c2b1827912d
Neko Ayaka <neko@ayaka.moe>
2024-12-02 16:46:00 +0800

    fix: make properties concurrent safe

--- pkg/properties/properties.go
+++ pkg/properties/properties.go
@@ -3,22 +3,50 @@ package properties
 import (
 	"context"
 	"errors"
+	"sync"
 )
 
 type propertiesKey struct{}
 
-type pp map[string]interface{}
+type property struct {
+	mutex sync.RWMutex
+	pp    map[string]any
+}
+
+func newProperty() *property {
+	return &property{
+		pp: make(map[string]any),
+	}
+}
+
+func (p *property) Set(key string, value interface{}) {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	p.pp[key] = value
+}
+
+func (p *property) Get(key string) (any, bool) {
+	p.mutex.RLock()
+	defer p.mutex.RUnlock()
+
+	value, exists := p.pp[key]
+	if !exists {
+		return nil, false
+	}
+
+	return value, true
+}
 
 // fromPropertiesContext 从 context 中获取所有属性
-func fromPropertiesContext(ctx context.Context) (pp, bool) {
-	props, ok := ctx.Value(propertiesKey{}).(pp)
+func fromPropertiesContext(ctx context.Context) (*property, bool) {
+	props, ok := ctx.Value(propertiesKey{}).(*property)
 	return props, ok
 }
 
 // NewPropertiesContext 初始化 PropertiesContext
 func NewPropertiesContext(ctx context.Context) context.Context {
-	props := make(pp)
-	return context.WithValue(ctx, propertiesKey{}, props)
+	return context.WithValue(ctx, propertiesKey{}, newProperty())
 }
 
 // SetProperty 设置任意类型的值到 context 中
@@ -28,7 +56,7 @@ func SetProperty(ctx context.Context, key string, value interface{}) error {
 		return errors.New("context does not have properties space, please use NewPropertiesContext to initialize it")
 	}
 	// update old ctx
-	props[key] = value
+	props.Set(key, value)
 
 	return nil
 }
@@ -42,7 +70,7 @@ func GetProperty[T any](ctx context.Context, key string) (T, bool) {
 		return zero, false
 	}
 
-	value, exists := props[key]
+	value, exists := props.Get(key)
 	if !exists {
 		return zero, false
 	}

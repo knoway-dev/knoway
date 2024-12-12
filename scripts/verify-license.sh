3e53737ffa91634a253cc2dd4608a8fade7c72cd
nicole-lihui <nicole.li@daocloud.io>
2024-12-12 19:47:06 +0800

    remove license

--- scripts/verify-license.sh
+++ scripts/verify-license.sh
@@ -5,9 +5,9 @@ set -ex
 CONFIG_PATH=${CONFIG_PATH:-$(dirname "${BASH_SOURCE[0]}")/..}
 
 if license-lint -config ${CONFIG_PATH}/license-lint.yml; then
-    echo "license lint succeeded"
+    echo "✅ license lint succeeded"
 else
     echo # print one empty line, separate from warning messages.
-    echo 'Please review the above error messages.'
+    echo '❌ Please review the above error messages.'
     exit 1
 fi

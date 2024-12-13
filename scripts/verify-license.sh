25b645edd993f0348452c03d948d1a3c85b7280f
Neko aka Fanshi <nekoayaka.zhang@daocloud.io>
2024-12-13 14:31:49 +0800

    Apply 1 suggestion(s) to 1 file(s)

--- scripts/verify-license.sh
+++ scripts/verify-license.sh
@@ -5,7 +5,7 @@ set -ex
 CONFIG_PATH=${CONFIG_PATH:-$(dirname "${BASH_SOURCE[0]}")/..}
 
 if license-lint -config ${CONFIG_PATH}/license-lint.yml; then
-    echo "✅ license lint succeeded"
+    echo "✅ License lint succeeded"
 else
     echo # print one empty line, separate from warning messages.
     echo '❌ Please review the above error messages.'
